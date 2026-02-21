b3a9b1fe1e37ed66441888f716c321b05c87040b
Rakesh Kumar Raushan <rakraushan@deloitte.com>
2026-02-21 14:07:45 +0530

    Initial Commit

---
+++ ToastDemo/ToastDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToastDemoApp.swift
+//  ToastDemo
+//
+//  Created by Raushan, Rakesh Kumar on 21/02/26.
+//
+
+import SwiftUI
+
+@main
+struct ToastDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
