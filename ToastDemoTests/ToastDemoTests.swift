a6c870eaba951b641b3ccbd5800053f7c4384b2b
Rakesh Kumar Raushan <rakraushan@deloitte.com>
2026-02-22 09:18:18 +0530

    Adding Tests Scheme

---
+++ ToastDemoTests/ToastDemoTests.swift
@@ -0,0 +1,16 @@
+//
+//  ToastDemoTests.swift
+//  ToastDemoTests
+//
+//  Created by Raushan, Rakesh Kumar on 22/02/26.
+//
+
+import Testing
+
+struct ToastDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
